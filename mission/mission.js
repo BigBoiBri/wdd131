4e46e27f0d27d2438864b81587499adbf6dccf53
BigBoiBri <brianskousenmusic@gmail.com>
2025-05-03 15:59:17 -0600

    Fix

--- mission/mission.js
+++ mission/mission.js
@@ -3,15 +3,11 @@ let logo = document.getElementById('logo');
 
 selectElem.addEventListener('change', changeTheme);
 
-
-
 function changeTheme() {
     let current = selectElem.value;
-
     if (current == 'dark'){
         document.body.className = 'dark';
         logo.src = 'byui-logo_white.png';
-
     } if (current == 'light') {
         document.body.className = 'light';
         logo.src = 'byui-logo_blue.webp';
