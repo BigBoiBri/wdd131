08e4070ab67e5deeee3e7b8b0f43d8eca021c882
BigBoiBri <brianskousenmusic@gmail.com>
2025-05-16 17:09:46 -0600

    May 16

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -4,3 +4,32 @@ const navLinks = document.getElementById('navLinks');
 menuButton.addEventListener('click', function () {
   navLinks.classList.toggle('show');
 });
+
+const modal = document.createElement('dialog');
+modal.innerHTML = `
+  <img src="" alt="Large view">
+  <button class="close-viewer">X</button>`;
+document.body.appendChild(modal);
+
+const modalImg = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+const gallery = document.querySelector('.gallery');
+
+gallery.addEventListener('click', function (event) {
+  const img = event.target.closest('img');
+  if (img) {
+    modalImg.src = img.src.replace('norris-sm.jpeg', 'norris-full.jpeg'); // or just use img.src if no large version
+    modal.showModal();
+  }
+});
+
+modal.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.close();
+  }
+});
+
+closeButton.addEventListener('click', () => {
+  modal.close();
+});
